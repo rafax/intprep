a7146694d40c2c5c9cf85d55fbe47617dcc3007d
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2017-07-23 22:50:50 +0200

    Davis

---
+++ src/test/java/com/gajdulewicz/intprep/DavisStaircaseTest.java
@@ -0,0 +1,28 @@
+package com.gajdulewicz.intprep;
+
+import com.google.common.collect.Maps;
+import com.google.common.truth.Truth;
+import org.junit.Test;
+
+import static com.gajdulewicz.intprep.DavisStaircase.ways;
+
+/**
+ * Created by gajduler on 7/23/17.
+ */
+public class DavisStaircaseTest {
+
+    @Test
+    public void test1() {
+        Truth.assertThat(ways(1, Maps.newHashMap())).isEqualTo(1);
+    }
+
+    @Test
+    public void test2() {
+        Truth.assertThat(ways(2, Maps.newHashMap())).isEqualTo(2);
+    }
+
+    @Test
+    public void test3() {
+        Truth.assertThat(ways(3, Maps.newHashMap())).isEqualTo(4);
+    }
+}
