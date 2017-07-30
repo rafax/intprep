3bb465123c2046cda679955c515a637c71c855d1
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2017-07-30 14:20:39 +0200

    2.5 done

--- src/main/java/com/gajdulewicz/intprep/ctci/LinkedLists.java
+++ src/main/java/com/gajdulewicz/intprep/ctci/LinkedLists.java
@@ -5,9 +5,106 @@ import com.google.common.collect.Sets;
 
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.stream.Stream;
+import java.util.List;
 
 public class LinkedLists {
+
+  // 2.1
+  public static void removeDuplicates(Node<Integer> head) {
+    if (head == null) {
+      return;
+    }
+    final Iterator<Node<Integer>> nodes = head.nodes();
+    Node<Integer> prev = nodes.next(), curr = null;
+    HashSet<Integer> seen = Sets.newHashSet(prev.elem);
+    while (nodes.hasNext()) {
+      curr = nodes.next();
+      if (seen.contains(curr.elem)) {
+        prev.next = curr.next;
+      } else {
+        seen.add(curr.elem);
+        prev = curr;
+      }
+    }
+  }
+
+  // 2.2
+  static <T> Node<T> kthLast(Node<T> head, int k) {
+    if (head == null) {
+      return null;
+    }
+    Node<T> left = head, right = head;
+    for (int i = 0; i < k - 1; i++) {
+      if (right.next == null) {
+        return null;
+      }
+      right = right.next;
+    }
+    while (right.next != null) {
+      right = right.next;
+      left = left.next;
+    }
+    return left;
+  }
+
+  // 2.4
+  static Node<Integer> partition(Node<Integer> head, int pivot) {
+    if (head == null) {
+      return null;
+    }
+    Node<Integer> lte = null, gt = null, curr = head;
+    while (curr != null) {
+      if (curr.elem <= pivot) {
+        if (lte == null) {
+          lte = new Node<>(curr.elem);
+        } else {
+          lte.add(curr.elem);
+        }
+      } else {
+        if (gt == null) {
+          gt = new Node<>(curr.elem);
+        } else {
+          gt.add(curr.elem);
+        }
+      }
+      curr = curr.next;
+    }
+    final Node<Integer> last = Iterators.getLast(lte.nodes());
+    last.next = gt;
+    return lte;
+  }
+
+  // 2.5 - only numbers of equal length
+  // assuming MSD last
+  public static Node<Integer> add(Node<Integer> left, Node<Integer> right) {
+    if (left == null || right == null) {
+      return null;
+    }
+    Node<Integer> res = null;
+    final Iterator<Integer> li = left.elements();
+    final Iterator<Integer> ri = right.elements();
+    int carry = 0;
+    while (li.hasNext()) {
+      final Integer l = li.next();
+      final Integer r = ri.next();
+      int n = l + r + carry;
+      if (n >= 10) {
+        carry = 1;
+        n = n % 10;
+      } else {
+        carry = 0;
+      }
+      if (res == null) {
+        res = new Node<>(n);
+      } else {
+        res.add(n);
+      }
+    }
+    if (carry > 0) {
+      res.add(1);
+    }
+    return res;
+  }
 
   static class Node<T> {
     final T elem;
@@ -15,6 +112,16 @@ public class LinkedLists {
 
     public Node(T elem) {
       this.elem = elem;
+    }
+
+    public Node(List<T> elems) {
+      if (elems.size() == 0) {
+        throw new RuntimeException("Array cannot be empty");
+      }
+      this.elem = elems.get(0);
+      for (int i = 1; i < elems.size(); i++) {
+        this.add(elems.get(i));
+      }
     }
 
     public void add(T elem) {
@@ -63,70 +170,5 @@ public class LinkedLists {
     public String toString() {
       return "Node{" + "elem=" + elem + ", next=" + next + '}';
     }
-  }
-
-  // 2.1
-  public static void removeDuplicates(Node<Integer> head) {
-    if (head == null) {
-      return;
-    }
-    final Iterator<Node<Integer>> nodes = head.nodes();
-    Node<Integer> prev = nodes.next(), curr = null;
-    HashSet<Integer> seen = Sets.newHashSet(prev.elem);
-    while (nodes.hasNext()) {
-      curr = nodes.next();
-      if (seen.contains(curr.elem)) {
-        prev.next = curr.next;
-      } else {
-        seen.add(curr.elem);
-        prev = curr;
-      }
-    }
-  }
-
-  // 2.2
-  static <T> Node<T> kthLast(Node<T> head, int k) {
-    if (head == null) {
-      return null;
-    }
-    Node<T> left = head, right = head;
-    for (int i = 0; i < k - 1; i++) {
-      if (right.next == null) {
-        return null;
-      }
-      right = right.next;
-    }
-    while (right.next != null) {
-      right = right.next;
-      left = left.next;
-    }
-    return left;
-  }
-
-  // 2.2
-  static Node<Integer> partition(Node<Integer> head, int pivot) {
-    if (head == null) {
-      return null;
-    }
-    Node<Integer> lte = null, gt = null, curr = head;
-    while (curr != null) {
-      if (curr.elem <= pivot) {
-        if (lte == null) {
-          lte = new Node<>(curr.elem);
-        } else {
-          lte.add(curr.elem);
-        }
-      } else {
-        if (gt == null) {
-          gt = new Node<>(curr.elem);
-        } else {
-          gt.add(curr.elem);
-        }
-      }
-      curr = curr.next;
-    }
-    final Node<Integer> last = Iterators.getLast(lte.nodes());
-    last.next = gt;
-    return lte;
   }
 }
--- src/test/java/com/gajdulewicz/intprep/ctci/LinkedListsTest.java
+++ src/test/java/com/gajdulewicz/intprep/ctci/LinkedListsTest.java
@@ -1,17 +1,13 @@
 package com.gajdulewicz.intprep.ctci;
 
 import com.google.common.collect.Lists;
-import com.google.common.collect.Range;
 import com.google.common.truth.Truth;
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static com.gajdulewicz.intprep.ctci.LinkedLists.kthLast;
-import static com.gajdulewicz.intprep.ctci.LinkedLists.partition;
-import static com.gajdulewicz.intprep.ctci.LinkedLists.removeDuplicates;
+import static com.gajdulewicz.intprep.ctci.LinkedLists.*;
 
 public class LinkedListsTest {
 
@@ -27,8 +23,8 @@ public class LinkedListsTest {
 
   @Test
   public void removeDuplicatesTest() {
-    LinkedLists.Node<Integer> head = new LinkedLists.Node<>(0);
-    Lists.newArrayList(1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 0).forEach(head::add);
+    LinkedLists.Node<Integer> head =
+        new LinkedLists.Node<>(Lists.newArrayList(0, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 0));
     removeDuplicates(head);
     Truth.assertThat(Lists.newArrayList(head.elements()))
         .isEqualTo(Lists.newArrayList(0, 1, 2, 3, 4, 5));
@@ -36,8 +32,7 @@ public class LinkedListsTest {
 
   @Test
   public void kthLastTest() {
-    LinkedLists.Node<Integer> head = new LinkedLists.Node<>(0);
-    Lists.newArrayList(1, 2, 3, 4, 5).forEach(head::add);
+    LinkedLists.Node<Integer> head = new LinkedLists.Node<>(Lists.newArrayList(0, 1, 2, 3, 4, 5));
     for (int i = 1; i <= 6; i++) {
       Truth.assertThat(kthLast(head, i).elem).isEqualTo(6 - i);
     }
@@ -45,10 +40,26 @@ public class LinkedListsTest {
 
   @Test
   public void partitionTest() {
-    LinkedLists.Node<Integer> head = new LinkedLists.Node<>(4);
-    Lists.newArrayList(1, 5, 3, 2, 7, 6).forEach(head::add);
+    LinkedLists.Node<Integer> head =
+        new LinkedLists.Node<>(Lists.newArrayList(4, 1, 5, 3, 2, 7, 6));
     final LinkedLists.Node<Integer> res = partition(head, 4);
     Truth.assertThat(Lists.newArrayList(res.elements()))
         .isEqualTo(Lists.newArrayList(4, 1, 3, 2, 5, 7, 6));
+  }
+
+  @Test
+  public void addTest() {
+    LinkedLists.Node<Integer> left = new LinkedLists.Node<>(Lists.newArrayList(7, 1, 6));
+    LinkedLists.Node<Integer> right = new LinkedLists.Node<>(Lists.newArrayList(5, 9, 2));
+    final LinkedLists.Node<Integer> res = add(left, right);
+    Truth.assertThat(Lists.newArrayList(res.elements())).isEqualTo(Lists.newArrayList(2, 1, 9));
+  }
+
+  @Test
+  public void addWithOverflowTest() {
+    LinkedLists.Node<Integer> left = new LinkedLists.Node<>(Lists.newArrayList(7, 1, 6));
+    LinkedLists.Node<Integer> right = new LinkedLists.Node<>(Lists.newArrayList(5, 9, 4));
+    final LinkedLists.Node<Integer> res = add(left, right);
+    Truth.assertThat(Lists.newArrayList(res.elements())).isEqualTo(Lists.newArrayList(2, 1, 1, 1));
   }
 }
